2ec4cf7936496864047c4c5c609f5dff293fa605
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2024-11-28 22:15:56 +0800

    addded aqi

--- models/History.js
+++ models/History.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const historySchema = new mongoose.Schema({
     date: String,
     timestamp: String,
+    aqi: Number,
     pm2_5: Number,
     co2: Number,
     no2: Number,
