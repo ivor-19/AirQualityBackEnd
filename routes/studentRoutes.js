b267d4adbab4debb7b4997dfb7467d325b6ca560
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2024-12-11 23:16:20 +0800

    dfs

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getStudentList, addStudentData, deleteStudent } = require('../controllers/studentController');
+const { getStudentList, deleteStudent, addStudent } = require('../controllers/studentController');
 const router = express.Router();
 
 router.get('/getList', getStudentList);
-router.post('/addStudent', addStudentData);
+router.post('/addStudent', addStudent);
 router.post('/deleteStudent/:id', deleteStudent); // Added `:id` for delete route
 
 module.exports = router;  // Corrected export
