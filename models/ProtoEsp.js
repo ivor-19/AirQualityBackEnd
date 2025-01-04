af955b3f536f0f56f608368f45e2a7481053a910
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-01-04 15:24:00 +0800

    wowowow

--- models/ProtoEsp.js
+++ models/ProtoEsp.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const protoSchema =  new mongoose.Schema({
-  name: {type: String}
+  temperature: {type: Number},
+  humidity: {type: Number}
 })
 const ProtoEsp = mongoose.model('ProtoEsp', protoSchema);
 module.exports = ProtoEsp;
