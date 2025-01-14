e2d620210c43a73cfda19d9c165e800f2011c3c3
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-01-14 15:17:36 +0800

    date

--- models/AirQualityReading.js
+++ models/AirQualityReading.js
@@ -2,18 +2,22 @@ const mongoose = require('mongoose');
 const moment = require('moment-timezone');
 
 const airQualityReadingSchema = new mongoose.Schema({
-    aqi: Number,
-    pm2_5: Number,
-    co: Number,
-    no2: Number,
-    asset_model: String,
-    status: {
-      type: String,
-      enum: ['on', 'off'],  // Only 'on' or 'off' are allowed
-      default: 'off',       // Set the default value as 'off'
-    },
-    last_updated: { type: String, default: moment().tz('Asia/Manila').format('YYYY-MM-DDTHH:mm:ss.SSSZ') }
-  });
+  aqi: Number,
+  pm2_5: Number,
+  co: Number,
+  no2: Number,
+  asset_model: String,
+  status: {
+    type: String,
+    enum: ['on', 'off'],
+    default: 'off',
+  },
+  last_updated: { 
+    type: Date, 
+    default: () => moment().tz('Asia/Manila').toDate()  // Use moment to get a Date object
+  }
+});
+
   
 const AirQualityReading = mongoose.model('AirQualityReading', airQualityReadingSchema);
 module.exports = AirQualityReading;
