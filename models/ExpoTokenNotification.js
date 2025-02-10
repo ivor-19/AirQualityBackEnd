6226c4d372f7be9d419b04ef148d04a36d473d71
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-02-10 14:27:20 +0800

    fix

--- models/ExpoTokenNotification.js
+++ models/ExpoTokenNotification.js
@@ -4,5 +4,5 @@ const expoTokenNotificationSchema = new mongoose.Schema({
     token_notif: String,
   });
   
-const ExpoTokenNotification = mongoose.model('ExpoTokenNotifcation', expoTokenNotificationSchema);
+const ExpoTokenNotification = mongoose.model('ExpoTokenNotification', expoTokenNotificationSchema);
 module.exports = ExpoTokenNotification;
