8ded83f750d796fd6c69a05b1d227af98bded8e0
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-02-12 11:54:53 +0800

    added duplication checking in token_notif

--- controllers/expoTokenController.js
+++ controllers/expoTokenController.js
@@ -15,16 +15,28 @@ const getToken = async (req, res) => {
 }
 
 const postToken = async (req, res) => {
-    const {token_notif} = req.body;
-    const newToken = new ExpoTokenNotification({token_notif});
+    const { token_notif } = req.body;
 
     try {
+        const existingToken = await ExpoTokenNotification.findOne({ token_notif });
+        if (existingToken) {
+            return res.status(200).json({
+                isSuccess: true,
+                message: 'Token already exists, no action taken.',
+            });
+        }
+
+        const newToken = new ExpoTokenNotification({ token_notif });
         await newToken.save();
-        const getToken = await ExpoTokenNotification.find();
-        res.status(201).json({ isSuccess: true, message: 'New token is saved: ', getToken})
+
+        res.status(201).json({
+            isSuccess: true,
+            message: 'New token saved successfully',
+        });
     } catch (error) {
-        res.status(500).json({ isSuccess: false, message: 'Error saving token: ', error})
+        res.status(500).json({ isSuccess: false, message: 'Error saving token', error });
     }
-}
+};
+
 
 module.exports = {getToken, postToken};
