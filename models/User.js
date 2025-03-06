3456bc39c1f7b84a3a9e42f8f6b0ce8314d029fa
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-03-06 16:12:16 +0800

    asdsad

--- models/User.js
+++ models/User.js
@@ -14,11 +14,25 @@ const userSchema = new mongoose.Schema({
     asset_model: {type: String, default: " "},
     first_access: {type: String, default: "Yes"},
     device_notif: {type: String, default: " "},
-    created_at: {type: String, default: philippineTimeFull},
-    updated_at: {type: String, default: philippineTimeFull}
+    created_at: {
+        type: String, 
+        default: function() {
+            return moment().tz('Asia/Manila').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+        }
+    },
+    updated_at: {
+        type: String, 
+        default: function() {
+            return moment().tz('Asia/Manila').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+        }
+    }
 });
   
 userSchema.pre('save', async function(next) {
+    if (!this.isNew) {
+        this.updated_at = moment().tz('Asia/Manila').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+    }
+
     if(this.isModified('password')){
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
