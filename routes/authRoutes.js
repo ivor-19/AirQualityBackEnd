c35855f8d2f885b1f08f73e13243efe8dc645161
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-05 14:44:12 +0800

    Adas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/', getUsers)
+router.get('/emails', getEmails) 
 router.post('/editUser/:id', editUser)
 router.post('/deleteUser/:id', deleteUser) // Delete
 router.get('/:id', getSpecificUser) 
-router.get('/emails', getEmails) 
 
 module.exports = router;
