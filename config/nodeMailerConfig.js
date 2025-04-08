c465558d345884934f71b73abf4cfd18196de8af
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-08 14:20:41 +0800

    dfsdfdf

--- config/nodeMailerConfig.js
+++ config/nodeMailerConfig.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   auth: {
     // user: process.env.EMAIL_USER, // Use environment variables for sensitive data
     // pass: process.env.EMAIL_PASSWORD,
-    user: process.env.EMAIL_USER_AG, // Use environment variables for sensitive data
-    pass: process.env.EMAIL_PASSWORD_AG,
+    user: "airguard.alert@gmail.com", // Use environment variables for sensitive data
+    pass: "dolhjgfnntkrijyg",
   },
 });
 
