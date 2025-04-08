67697fea8d19fb9929017016cbecd8414017920a
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-08 13:19:32 +0800

    app password

--- config/nodeMailerConfig.js
+++ config/nodeMailerConfig.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
+  const nodemailer = require('nodemailer');
 
-// Create a transporter object using Gmail service (You can replace it with other services like Mailtrap or SMTP)
-const transporter = nodemailer.createTransport({
-  service: 'gmail', // You can change this if you are using a different service
-  auth: {
-    // user: process.env.EMAIL_USER, // Use environment variables for sensitive data
-    // pass: process.env.EMAIL_PASSWORD,
-    user: "airguard.alert@gmail.com",
-    password: "airguardalert2025",
-  },
-});
+  // Create a transporter object using Gmail service (You can replace it with other services like Mailtrap or SMTP)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // You can change this if you are using a different service
+    auth: {
+      // user: process.env.EMAIL_USER, // Use environment variables for sensitive data
+      // pass: process.env.EMAIL_PASSWORD,
+      user: "airguard.alert@gmail.com",
+      password: "dolh jgfn ntkr ijyg",
+    },
+  });
 
-module.exports = transporter;
+  module.exports = transporter;
