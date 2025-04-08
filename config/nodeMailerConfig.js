cbaaefb99daae59510aab56060c7170d303131d7
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-08 13:52:41 +0800

    adfsdf

--- config/nodeMailerConfig.js
+++ config/nodeMailerConfig.js
@@ -2,11 +2,14 @@
 
   // Create a transporter object using Gmail service (You can replace it with other services like Mailtrap or SMTP)
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // You can change this if you are using a different service
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.EMAIL_USER_AG, // Use environment variables for sensitive data
+      user: process.env.EMAIL_USER_AG,
       pass: process.env.EMAIL_PASSWORD_AG,
-    },
+    }
   });
 
   module.exports = transporter;
