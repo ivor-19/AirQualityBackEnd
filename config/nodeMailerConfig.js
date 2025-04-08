3e431905706404e20937c8a70cb03ee8d490d8e1
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-08 14:04:34 +0800

    dfsdf

--- config/nodeMailerConfig.js
+++ config/nodeMailerConfig.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail', // You can change this if you are using a different service
   auth: {
-    user: process.env.EMAIL_USER, // Use environment variables for sensitive data
-    pass: process.env.EMAIL_PASSWORD,
+    user: "airguard.alert@gmail.com", // Use environment variables for sensitive data
+    pass: "dolhjgfnntkrijyg",
   },
 });
 
