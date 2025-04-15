473c3ac8ba1d946c66e83fa92edb5dd7d073e9ed
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-16 00:38:09 +0800

    adasd

--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -1,11 +1,12 @@
 // routes/emailRoutes.js
 const express = require('express');
-const { getIssueList, postIssue, updateIssueStatus } = require('../controllers/issueController');
+const { getIssueList, postIssue, updateIssueStatus, updateIssueComment } = require('../controllers/issueController');
 
 const router = express.Router();
 
 router.get('/', getIssueList);
 router.post('/', postIssue);
 router.post('/:id/status', updateIssueStatus);
+router.post('/:id/comment', updateIssueComment);
 
 module.exports = router;
