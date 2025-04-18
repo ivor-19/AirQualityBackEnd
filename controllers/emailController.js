9e344a10852760eab622b7a1f54e11f5ef47888d
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-04-18 15:52:27 +0800

    html

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,21 +1,22 @@
 const transporter = require('../config/nodeMailerConfig');
 
 const sendEmail = async (req, res) => {
-  const { to, subject, message } = req.body;
+  const { to, subject, message, html } = req.body;
 
   // Validate the inputs
-  if (!to || !subject || !message) {
+  if (!to || !subject || (!message && !html)) {
     return res.status(400).json({
       success: false,
-      message: 'Please provide "to", "subject", and "message".',
+      message: 'Please provide "to", "subject", and either "message" or "html".',
     });
   }
 
   const mailOptions = {
-    from: process.env.EMAIL_USER_AG, 
+    from: process.env.EMAIL_USER_AG,
     to,
     subject,
-    text: message, 
+    text: message || undefined,  // Only include if provided
+    html: html || undefined,     // Only include if provided
   };
 
   try {
@@ -32,5 +33,42 @@ const sendEmail = async (req, res) => {
   }
 };
 
-
 module.exports = { sendEmail };
+
+
+// const transporter = require('../config/nodeMailerConfig');
+
+// const sendEmail = async (req, res) => {
+//   const { to, subject, message } = req.body;
+
+//   // Validate the inputs
+//   if (!to || !subject || !message) {
+//     return res.status(400).json({
+//       success: false,
+//       message: 'Please provide "to", "subject", and "message".',
+//     });
+//   }
+
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER_AG, 
+//     to,
+//     subject,
+//     text: message, 
+//   };
+
+//   try {
+//     const info = await transporter.sendMail(mailOptions);
+//     return res.status(200).json({
+//       success: true,
+//       message: `Email sent: ${info.response}`,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({
+//       success: false,
+//       message: `Error: ${error.message}`,
+//     });
+//   }
+// };
+
+
+// module.exports = { sendEmail };
